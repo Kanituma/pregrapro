411a238f2339d6c9bcb188c4629aaed6c70767a8
Kanituma <zhangyuanxiao@cuc.edu.cn>
2020-05-24 14:38:04 +0800

    拼图svg origin解决
    SVG width 和height 都以左上角为原点缩放，无法更改 ->
    解决方法：用translate(x,y)抵消（+transition），再修改origX，origY

--- js/puzzle.js
+++ js/puzzle.js
@@ -1,19 +1,23 @@
 /*Programacion de JavaScript*/
 
-var pieces = document.getElementsByClassName('move');
+var moves = document.getElementsByClassName('move');
 
-var initWidth = [279/6,192/3,134/3,163/3,134/3,163/3,134/3];
-var initHeight = [190/6,134/3,163/3,134/3,192/3,134/3,163/3];
+var initWidth = [178.83/3,135.25/3,135.89/3,177.56/3,178.84/3,134.61/3];
+var initHeight = [178.83/3,135.25/3,135.89/3,177.56/3,178.84/3,134.61/3];
 var lastWidth = [178.83,135.25,135.89,177.56,178.84,134.61];
 var lastHeight = [121.79,121.79,161.53,161.53,160.89,161.53];
-for(var i=0;i<pieces.length;i++){
-	pieces[i].setAttribute("width", initWidth[i]);
-	pieces[i].setAttribute("height",initHeight[i]);
-	pieces[i].setAttribute("x", 50+Math.floor(50*i));
-	pieces[i].setAttribute("y", Math.floor(570));
-	// pieces[i].setAttribute("onmousedown","seleccionarElemento(evt)");
-	pieces[i].setAttribute("ontouchstart","seleccionarElemento(evt)");
-	pieces[i].setAttribute("ontouchmove","moveElement(evt)");
+var transX = [-50,-50,-45,-65,-40,-40];
+var transY = [-30,-30,-60,-57,-60,-60];
+for(var i=0;i<moves.length;i++){
+	// moves[i].setAttribute("width", initWidth[i]);
+	// moves[i].setAttribute("height",initHeight[i]);
+	moves[i].setAttribute("width", initWidth[i]);
+	moves[i].setAttribute("height",initHeight[i]);
+	moves[i].setAttribute("x", 50+Math.floor(50*i));
+	moves[i].setAttribute("y", Math.floor(570));
+	// moves[i].setAttribute("onmousedown","seleccionarElemento(evt)");
+	moves[i].setAttribute("ontouchstart","seleccionarElemento(evt)");
+	moves[i].setAttribute("ontouchmove","moveElement(evt)");
 }
 
 var elementSelect = 0;  
@@ -23,7 +27,7 @@ var currentPosX = 0;
 var currentPosY = 0;
 
 function seleccionarElemento(evt) {
-	console.log("selecccionr")
+	// console.log("selecccionr")
 	// console.log("sdfsdfsdf")
 	// console.log(evt.target.getAttribute("x"))
 	elementSelect = reorg(evt);
@@ -38,32 +42,41 @@ function seleccionarElemento(evt) {
 }
 
 function moveElement(evt){
+	var flag = false
 	var i = elementSelect.id.slice(5) -1
-	console.log(i)
+	console.log()
+	// if(flag == false){
+	// 	var i = elementSelect.id.slice(5) -1
+	// 	var w = lastWidth[i];
+	// 	var h = lastWidth[i];
+	// 	console.log("w = "+w+",h="+h)
+	// 	elementSelect.setAttribute("x",currentPosx-w);
+	// 	elementSelect.setAttribute("y",currentPosy-h);
+	// 	elementSelect.setAttribute("width",lastWidth[i])
+	// 	elementSelect.setAttribute("height",lastHeight[i])
+	// 	flag = true
+	// }
+	// elementSelect.setAttribute("width",lastWidth[i])
+	// elementSelect.setAttribute("height",lastHeight[i])
 	elementSelect.setAttribute("width",lastWidth[i])
 	elementSelect.setAttribute("height",lastHeight[i])
-	console.log("moveele")
-	// console.log("sdf")
-	// console.log(elementSelect)
-	// console.log(evt.changedTouches[0].clientX)
+	elementSelect.setAttribute("transform","translate("+transX[i]+" "+transY[i]+")")
 	var dx = evt.changedTouches[0].clientX - currentX;
 	var dy = evt.changedTouches[0].clientY - currentY;
 	currentPosx = currentPosx + dx;
 	currentPosy = currentPosy + dy;
-	// elementSelect = reorg(evt);
-	console.log("x="+elementSelect.getAttribute("x")+",y="+elementSelect.getAttribute("y"))
+	// console.log("x="+elementSelect.getAttribute("x")+",y="+elementSelect.getAttribute("y"))
 	elementSelect.setAttribute("x",currentPosx);
 	elementSelect.setAttribute("y",currentPosy);
 	
 	currentX = evt.changedTouches[0].clientX;        
 	currentY = evt.changedTouches[0].clientY;
-	// elementSelect.setAttribute("onmouseout","deseleccionarElemento(evt)");
 	elementSelect.setAttribute("ontouchend","deseleccionarElemento(evt)");
 	magnet();
 }
 
 function deseleccionarElemento(evt){
-	console.log("deseleccionar")
+	// console.log("deseleccionar")
 	testing();
 	if(elementSelect != 0){			
 		elementSelect.removeAttribute("ontouchmove");
@@ -76,7 +89,7 @@ function deseleccionarElemento(evt){
 var puzzle = document.getElementById('puzzle');
 
 function reorg(evt){
-	console.log("reorg")
+	// console.log("reorg")
 	var piece = evt.target.parentNode;
 	var clone = piece.cloneNode(true);
 	var id = piece.getAttribute("id");
@@ -92,10 +105,18 @@ var origX = [65.65,200.26,65.65,158,64.65,200.2];
 var origY = [169.5,169.5,250.55,250.55,370.55,370.55];
 function magnet(){
 	var i= elementSelect.id.slice(5) -1;
-	if (Math.abs(currentPosx-origX[i])<10 && Math.abs(currentPosy-origY[i])<10) {
-		elementSelect.setAttribute("x",origX[i]);
-		elementSelect.setAttribute("y",origY[i]);
+	norigX = origX[i]-transX[i];
+	norigY = origY[i]-transY[i];
+	console.log(currentPosx-norigX)
+	console.log(currentPosy-norigY)
+	if (Math.abs(currentPosx-norigX)<15 && Math.abs(currentPosy-norigY)<15) {
+		elementSelect.setAttribute("x",norigX);
+		elementSelect.setAttribute("y",norigY);
 	}
+	// if (Math.abs(currentPosx-origX[i])<15 && Math.abs(currentPosy-origY[i])<15) {
+	// 	elementSelect.setAttribute("x",origX[i]);
+	// 	elementSelect.setAttribute("y",origY[i]);
+	// }
 }
 			
 // var win = document.getElementById("win");
@@ -111,7 +132,15 @@ function testing() {
 			bien_ubicada = bien_ubicada + 1;
 		}
 	}
-	if(bien_ubicada == 9){
+	if(bien_ubicada == 6){
 		// win.play();
+		for(var i=0;i<pieces.length;i++){
+			console.log(moves[i])
+			for(var i=0;i<moves.length;i++){
+				moves[i].removeAttribute("ontouchstart")
+				moves[i].removeAttribute("ontouchmove")
+				moves[i].removeAttribute("ontouchend")
+			}
+		}
 	}
 }
