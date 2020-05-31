1e1da43a06fbf8bb674f95852503d42f0b0f1dd4
Kanituma <zhangyuanxiao@cuc.edu.cn>
2020-05-31 12:31:08 +0800

    5.30 拼图部分完成

--- js/puzzle.js
+++ js/puzzle.js
@@ -17,11 +17,12 @@ var transX = [-50,-50,-45,-65,-40,-40];
 var transY = [-30,-30,-60,-57,-60,-60];
 var winh = Math.floor(innerHeight/100)
 var gap = 0;
+var nowPuzzle;
+var puzzle = document.getElementById('puzzle');
 // console.log(pici)
 //initialize page
 if(winh>=8){
 	gap =25;
-	$("#p_title").css("top","16vh")
 }
 for(var i=0;i<moves.length;i++){
 	moves[i].setAttribute("width", initWidth[i]);
@@ -83,7 +84,7 @@ function deselectElement(evt){
 	}
 }
 
-var puzzle = document.getElementById('puzzle');
+
 
 function reorg(evt){
 	var piece = evt.target.parentNode;
@@ -95,7 +96,7 @@ function reorg(evt){
 }
 
 // var origX = [65.65,200.26,65.65,158,64.65,200.2];  
-var origX = [66.65,200.26,66.65,158,66.65,200.2];   
+var origX = [67.15,200.26,67.15,158,67.15,200.2];   
 var origY = [169.5,169.5,250.55,250.55,370.55,370.55];
 var isRight = [false,false,false,false,false,false]
 var norigX,norigY
@@ -124,32 +125,120 @@ function magnet(){
 		// moves[i].removeAttribute("ontouchend")
 	}
 }
+
+
+
+
+
+
+
+//信件
+var isTip = true;
 var num = 0;
 var right = document.getElementById("right")
+var delaytime =[9000,9000,6000,9000,6000,9000]
 function testing(){
+	var index = piece1.getAttribute("xlink:href").slice(5,6) -1
 	// console.log(isRight)
 	if(!isRight.includes(false)){
 		$("#p_gif").css("opacity","1")
 		$("#puzzle").delay(1500).fadeOut("slow");
-		$("#p_gif").css("animation","puzzleani 3s 2s steps(30) both")
+		$("#p_gif").css("animation-name","puzzleani")
+		setTimeout(function(){
+			letterOn();
+		},delaytime[index])
+
 		// console.log("win!")
 	}
+}
+function letterOn(){
+	$("#p_story").css("opacity","1")
+	$("#blackbg").css("opacity","0.3")
+	$("#p_tip").css("background-image","url('../img/puzzle_share.png')")
+	// $("#blackbg").css("pointer-events","all")
+	$("#p_tip").css("z-index","-3")
+	isTip = false; 
+	$("#p_back").css("z-index","-3")
+	$("#p_story").css("pointer-events","all")
+	setTimeout(function(){
+		$("#p_gif").css("animation-name","none")
+	},1000)
+	console.log("letterOn!")
+}
+function letterOff(){
+	$("#p_story").css("opacity","0")
+	$("#blackbg").css("opacity","0")
+	$("#p_story").css("pointer-events","none")
+	setTimeout(function(){
+		$("#p_tip").css("z-index","0")
+		$("#p_back").css("z-index","0")
+	},1000)
+	$("#p_gif").css("animation-name","puzzleani")
+	console.log("letterOff!")
 }
 
 //back tip button
 var backimg = document.getElementById("back");
 var p_back = document.getElementById("p_back");
 var p_tip = document.getElementById("p_tip");
+
 p_back.onclick = function(){
 	$(".page3").fadeOut("slow");
+	
+	$("#p_gif").css("opacity","0")
+	setTimeout(function(){
+		reloadPuzzle("reloadpuzzle")
+		$("#p_gif").css("animation-name","none")
+		puzzle.innerHTML = "<g id=\"back\" opacity=\"0\"><image xlink:href=\"img/p6.png\" width=\"268.7\" height=\"361\" x=\"65.65\" y=\"169.5\"></g><g id=\"backframe\"><image xlink:href=\"img/puzzle_lines.png\" width=\"268.7\" height=\"361\" x=\"65.65\" y=\"169.5\"></g><g class=\"piece\" id=\"0\"><image xlink:href=\"img/p6_1.png\" class=\"move\" id=\"piece1\"></image></g><g class=\"piece\" id=\"1\"><image xlink:href=\"img/p6_2.png\" class=\"move\" id=\"piece2\"></image></g><g class=\"piece\" id=\"2\"><image xlink:href=\"img/p6_3.png\" class=\"move\" id=\"piece3\"></image></g><g class=\"piece\" id=\"3\"><image xlink:href=\"img/p6_4.png\" class=\"move\" id=\"piece4\"></image></g><g class=\"piece\" id=\"4\"><image xlink:href=\"img/p6_5.png\" class=\"move\" id=\"piece5\"></image></g><g class=\"piece\" id=\"5\"><image xlink:href=\"img/p6_6.png\" class=\"move\" id=\"piece6\"></image></g>"
+	},1000)
+	
+	
 }
-p_tip.ontouchstart = function(){
-	timeOutEvent = setTimeout(function() {    
-		timeOutEvent = 0;    
-		backimg.setAttribute("opacity",1);  
-		// $("#back").fadeIn("slow")
-		setTimeout(function(){
-			backimg.setAttribute("opacity",0);   
-		},2000) 
-	}, 100);    
+
+
+
+
+
+p_tip.onclick = function(){
+	if(isTip){
+		timeOutEvent = setTimeout(function() {    
+			timeOutEvent = 0;    
+			backimg.setAttribute("opacity",1);  
+			// $("#back").fadeIn("slow")
+			setTimeout(function(){
+				backimg.setAttribute("opacity",0);   
+			},2000) 
+		}, 100);    
+	}else if(!isTip){
+		letterOn();
+	}
+	
+}
+
+function reloadPuzzle(id)
+{
+	isTip = true;
+    var oldjs = null; 
+    var t = null; 
+	var oldjs = document.getElementById(id);
+	var newjs = oldjs.src;
+    if(oldjs) oldjs.parentNode.removeChild(oldjs); 
+    var scriptObj = document.createElement("script"); 
+    scriptObj.src = newjs; 
+    scriptObj.type = "text/javascript"; 
+    scriptObj.id = id; 
+	document.getElementsByTagName("head")[0].appendChild(scriptObj);
+	
+	
+
+}
+
+var blackbg = document.getElementById("blackbg");
+var p_share = document.getElementById("p_share");
+blackbg.onclick = function(){
+	letterOff();
+	
+}
+p_share.onclick = function(){
+	console.log("share!")
 }
